3b8cd5fe5025a0959ce06ddfbaa3d5e7277a7feb
geiteadmiral <124919564+geiteadmiral@users.noreply.github.com>
2023-02-27 20:38:22 +0100

    mye tull

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import type { UserConfig } from 'vite'
 
 const config: UserConfig = {
   plugins: [sveltekit()],
-  ssr: {
-    noExternal: ['three', 'troika-three-text'],
-  },
 }
 
 export default config
